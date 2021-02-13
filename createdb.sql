19fcb2e438a7fac81ad78ed84a144901d642b408
Artem Voronov <artemkavoronov@gmail.com>
2021-02-13 20:48:09 +0400

    Переписал бд-часть

--- createdb.sql
+++ createdb.sql
@@ -1,15 +1,25 @@
-create table Users
-            (ID numeric(19,0) unique primary key
+create table Users            
+            (ID INTEGER  unique primary key 
             ,Brief varchar(50)
             );
+            
+
 create table Chats
-            (ID numeric(19,0) unique primary key);
+            (ID integer unique primary key);
+
+create table UserChatRelation
+            (ID integer unique primary key autoincrement
+            ,ChatID integer 
+            ,UserID integer
+            ,FOREIGN KEY (ChatID) references Chats(ID)
+            ,FOREIGN KEY (UserID) references Users(ID));
+            
 create table Operation
-            (ID    numeric(19,0) identity unique primary key
-            ,UFrom numeric(19,0) 
-            ,UTo   numeric(19,0)
+            (ID    integer unique primary key autoincrement
+            ,UFrom integer 
+            ,UTo   integer
             ,Qty   money
-            ,ChatID numeric(19,0)
+            ,ChatID integer
             ,FOREIGN KEY (UFrom)  references  Users(ID)
             ,FOREIGN KEY (UTo)    references  Users(ID)
             ,FOREIGN KEY (ChatID) references  Chats(ID)
--- db_worker.py
+++ db_worker.py
@@ -1,5 +1,51 @@
 import sqlite3
+import os
+from typing import Tuple, Dict, List
 
 conn = sqlite3.connect('veksel.db')
+cursor = conn.cursor()
 
-conn.close()
+def _init_db():
+    with open("createdb.sql", "r") as f:
+        sql = f.read();
+    cursor.executescript(sql)
+    conn.commit()
+
+def checkdb():
+    cursor.execute("select name from sqlite_master "
+                   "where type='table' and name='Users'")
+    table_exists = cursor.fetchall()
+    if table_exists:
+        return
+    _init_db()
+
+def insert(table: str, column_values: Dict):
+    columns = ', '.join(column_values.keys())
+    values = [tuple(column_values.values())]
+    placeholder = ', '.join("?" * len(column_values.keys()))
+    cursor.executemany(f"insert into {table}"
+                       f"({columns})"
+                       f"values({placeholder})"
+                       ,values)
+    conn.commit()
+
+def fetchall(table: str, columns: List[str]) -> List[Tuple]:
+    columns_joined = ', '.join(columns)
+    cursor.execute(f"select {columns_joined} from {table}")
+    rows = cursor.fetchall()
+    result = []
+    for row in rows:
+        dict_row = {}
+        for index, column in enumerate(columns):
+            dict_row[column] = row[index]
+        result.append(dict_row)
+    return result
+
+checkdb()
+
+print(fetchall('Users', {'ID', 'Brief'}))
+
+
+
+
+
