14a1ebbc4d1649359290f6b9b2c3cb384950136d
Artem Voronov <a.voronov@haulmont.com>
2021-08-07 17:52:43 +0400

    3 version - added supersaldo

--- bot/db_worker.py
+++ bot/db_worker.py
@@ -1,18 +1,21 @@
-import mysql.connector as connector
 import os
 from typing import Tuple, Dict, List
 
+import mysql.connector as connector
+
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+HOST = 'bot-sql-server'
+HOST = 'localhost'
+DB_PASSWORD = 'vekselbotpassword'
 
 
 def connect():
-    conn = connector.connect(host='bot-sql-server'
-                            ,database='veksel'
-                            ,user='root'
-                            ,password=DB_PASSWORD
-                            ,auth_plugin='mysql_native_password')
+    conn = connector.connect(host=HOST
+                             , database='veksel'
+                             , user='root'
+                             , password=DB_PASSWORD
+                             , auth_plugin='mysql_native_password')
     return conn
-
 
 
 def insert(table: str, column_values: Dict):
@@ -24,36 +27,37 @@ def insert(table: str, column_values: Dict):
     cursor.execute(f"insert into {table}"
                    f"({columns}) "
                    f"values({placeholder})"
-                   ,values)
+                   , values)
     conn.commit()
     lastrow = cursor.lastrowid
     cursor.close()
     conn.close()
     return lastrow
-    
 
 
-def update(table: str, id: int ,column_values: Dict):
+def update(table: str, id: int, column_values: Dict):
     conn = connect()
     cursor = conn.cursor()
     columns = column_values.keys()
-    values  = tuple(column_values.values())
+    values = tuple(column_values.values())
     columns_with_placeholders = ', '.join(x + ' = %s' for x in columns)
     cursor.execute(f"update {table}"
                    f"   set {columns_with_placeholders}"
                    f" where ID = {id}"
-                   ,values)
+                   , values)
     conn.commit()
     cursor.close()
     conn.close()
 
-def delete(table: str, row_id:int):
+
+def delete(table: str, row_id: int):
     conn = connect()
     cursor = conn.cursor()
     cursor.execute(f"delete from {table} where ID={row_id}")
     conn.commit()
     cursor.close()
     conn.close()
+
 
 def fetchall(table: str, columns: List[str]) -> List[Tuple]:
     conn = connect()
@@ -71,6 +75,7 @@ def fetchall(table: str, columns: List[str]) -> List[Tuple]:
         result.append(dict_row)
     return result
 
+
 def isExists(table: str, id: int) -> bool:
     conn = connect()
     cursor = conn.cursor()
@@ -82,6 +87,7 @@ def isExists(table: str, id: int) -> bool:
     cursor.close()
     conn.close()
     return res
+
 
 def isExistsRelation(chat_id: int, user_id: int) -> bool:
     conn = connect()
@@ -97,6 +103,7 @@ def isExistsRelation(chat_id: int, user_id: int) -> bool:
     conn.close()
     return res
 
+
 def getUsersForChat(id: int) -> List[Dict]:
     conn = connect()
     cursor = conn.cursor()
@@ -110,16 +117,18 @@ def getUsersForChat(id: int) -> List[Dict]:
     conn.close()
     return rows
 
+
 def getOperationsForChat(id: int) -> List[Dict]:
     conn = connect()
     cursor = conn.cursor()
     cursor.execute(f"select ID, UFrom, UTo, Qty, Comment, ChatID"
-                   f"  from Operation"                 
+                   f"  from Operation"
                    f" where ChatID = {id}")
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
     return rows
+
 
 def getOperationByID(id: int) -> Dict:
     conn = connect()
@@ -132,6 +141,7 @@ def getOperationByID(id: int) -> Dict:
     conn.close()
     return res
 
+
 def getOperationsBuf(id: int) -> Dict:
     conn = connect()
     cursor = conn.cursor()
@@ -141,11 +151,12 @@ def getOperationsBuf(id: int) -> Dict:
     conn.close()
     return res
 
+
 def getOperationsIdList(chat_id: int, user_id: int = 0) -> List[Dict]:
     conn = connect()
     cursor = conn.cursor()
     if user_id != 0:
-        cursor.execute(f'select ID as ID' 
+        cursor.execute(f'select ID as ID'
                        f'  from Operation'
                        f' where (UFrom = {user_id}'
                        f'    or UTo = {user_id})'
@@ -160,6 +171,7 @@ def getOperationsIdList(chat_id: int, user_id: int = 0) -> List[Dict]:
     cursor.close()
     conn.close()
     return res
+
 
 def getOperationText(operation_id: int) -> Dict:
     conn = connect()
@@ -176,3 +188,30 @@ def getOperationText(operation_id: int) -> Dict:
     conn.close()
     dt = {'UserFrom': res[0], 'UserTo': res[1], 'Qty': float(res[2]), 'Comment': res[3]}
     return dt
+
+
+def massDeleteOperations(chat_id: int):
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(f"delete from Operation where ChatID={chat_id}")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def getSuperSaldo(chat_id: int):
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(f"select ur.UserID"
+                   f"      ,sum(case when o.UTo = ur.UserID "
+                   f"                then -o.Qty else o.Qty end) "
+                   f"  from UserChatRelation ur "
+                   f"  join Operation o "
+                   f"    on o.ChatID = ur.ChatID "
+                   f"   and (ur.UserID in (o.UFrom, o.UTo)) "
+                   f" where ur.ChatID = {chat_id}"
+                   f" group by ur.UserID")
+    res = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return res
